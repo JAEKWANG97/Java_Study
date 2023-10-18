14aaf99dd3d902358035fc9134f3a3ea01cdcef3
Picka <marmong@naver.com>
2023-10-18 15:34:29 +0900

    update

--- src/ch02/sec02/FloatDoubleExample.java
+++ src/ch02/sec02/FloatDoubleExample.java
@@ -4,6 +4,6 @@ public class FloatDoubleExample {
     public static void main(String[] args) {
         // float과 double 타입의 소수 이하의 유효 자릿수를 확인한다.
         // double 타입은 float 타입 보다 약 2배의 유효 자릿수를 가지기 때문에 보다 정확한 데이터 저장이 가능하다.
-        // 이는 double이 float보다 약 2배의 정밀도를 갖는다는 의미에서 붙여진 것이다. 확인 후에는 10의 거즙 제곱 리터럴을 대입해서 출력해보자
+        // 이는 double이 float보다 약 2배의 정밀도를 갖는다는 의미에서 붙여진 것이다. 확인 후에는 10의 거듭 제곱 리터럴을 대입해서 출력해보자
     }
 }
